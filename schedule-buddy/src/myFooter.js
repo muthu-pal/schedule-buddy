0b3bf20e19e9651ece3265a8699269bc5ab2bb2a
muthupal <muthu@mpal.us>
2020-08-09 14:09:08 -0700

    tried to add header and footer

---
+++ schedule-buddy/src/myFooter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function myFooter() {
+    return (
+    <div className="footer">
+    <p className="left-side">made by ucla students :)</p>
+    <p className="right-side">privacy policy</p>
+    </div>
+  )
+}
+
+export default myFooter
---
+++ schedule-buddy/src/myHeader.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function myHeader() {
+  return (
+    <div>
+      <div className="header">
+      <p>Schedule Buddy</p>
+    </div>
+    <div className="upload-button">
+      <p>upload</p>
+    </div>
+    <div className="search-button">
+      <p className="search">search</p>
+    </div>
+    </div>
+  )
+}
+
+export default myHeader
