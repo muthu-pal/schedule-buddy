c07cfd8a7cbec85901135a29cfd85467384580df
muthu-pal <muthupal@g.ucla.edu>
2020-08-13 00:26:16 -0700

    finished nav bar except for styling

--- schedule-buddy/src/components/DropdownNav.js
+++ schedule-buddy/src/components/DropdownNav.js
@@ -1,34 +1,29 @@
-import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.css';
+
+ import 'bootstrap/dist/css/bootstrap.css';
+
+
+import React from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
 const DropdownNav = (props) => {
-  const [collapsed, setCollapsed] = useState(true);
-
-  const toggleNavbar = () => setCollapsed(!collapsed);
-
   return (
     <div>
-      <Navbar color="faded" light>
-        <NavbarBrand href="/" className="mr-auto"></NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
-            <NavItem>
-                <NavLink href="/">Home</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="/options">Options</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="/searchbyclass">Search By Class</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="/searchbyyear">Search By Year</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+      <p>List Based</p>
+      <Nav vertical className="ml-auto">
+        <NavItem>
+        <NavLink href="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
+        <NavLink href="/options">Options</NavLink>
+        </NavItem>
+        <NavItem>
+        <NavLink href="/searchbyclass">Search By Class</NavLink>
+        </NavItem>
+        <NavItem>
+        <NavLink href="/searchbyyear">Search By Year</NavLink>
+        </NavItem>
+      </Nav>
+      
     </div>
   );
 }
